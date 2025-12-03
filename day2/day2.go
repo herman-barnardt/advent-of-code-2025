d104a6a924ba506b9403e361240853ac79f99f1c
Herman Barnardt <hmbarnardt@gmail.com>
2025-12-03 06:58:12 +0200

    feat: Solve day 2

--- day2/day2.go
+++ day2/day2.go
@@ -1,6 +1,9 @@
 package day2
+
 import (
-	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 
 	aoc "github.com/herman-barnardt/aoc"
 )
@@ -10,9 +13,50 @@ func init() {
 }
 
 func solve2025Day2Part1(lines []string, test bool) interface{} {
-	return errors.New("Not yet implemented")
+	ranges := strings.Split(lines[0], ",")
+	sum := 0
+	for _, r := range ranges {
+		var start int
+		var end int
+		fmt.Sscanf(r, "%d-%d", &start, &end)
+		for i := start; i <= end; i++ {
+			str := strconv.Itoa(i)
+			if len(str)%2 == 0 && str[:len(str)/2] == str[len(str)/2:] {
+				sum += i
+			}
+		}
+
+	}
+	return sum
 }
 
 func solve2025Day2Part2(lines []string, test bool) interface{} {
-	return errors.New("Not yet implemented")
+	ranges := strings.Split(lines[0], ",")
+	sum := 0
+	for _, r := range ranges {
+		var start int
+		var end int
+		fmt.Sscanf(r, "%d-%d", &start, &end)
+		for i := start; i <= end; i++ {
+			str := strconv.Itoa(i)
+		sequenceLoop:
+			for j := 1; j <= len(str); j++ {
+				base := str[:j]
+				invalid := true
+				checked := false
+				if len(str)%j == 0 {
+					for x := j; x <= len(str)-j; x = x + j {
+						invalid = invalid && base == str[x:x+j]
+						checked = true
+					}
+					if invalid && checked {
+						sum += i
+						break sequenceLoop
+					}
+				}
+			}
+		}
+
+	}
+	return sum
 }
